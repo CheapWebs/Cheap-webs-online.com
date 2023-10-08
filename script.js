2e5a810e3cefbc586551eccd660297005ed5d459
CheapWebs <147328629+CheapWebs@users.noreply.github.com>
2023-10-08 17:00:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+setInterval(checkbox, 100);
+
+function checkbox(){
+
+    if(document.getElementById("check").checked == false){
+        document.getElementById("sendbut").disabled = true;   
+    }
+    else
+    document.getElementById("sendbut").disabled = false;   
+}
